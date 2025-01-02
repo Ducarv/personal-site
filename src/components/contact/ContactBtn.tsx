56743f61a158bd2d1abe20c5b1da8267fa789859
ducarv <educarvalho009@gmail.com>
2025-01-02 09:35:26 -0400

    contact button

--- src/components/contact/ContactBtn.tsx
+++ src/components/contact/ContactBtn.tsx
@@ -1,10 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const ContactBtn = () => {
     return (
         <section className="p-6 text-secondary bg-[#212529] mb-6 rounded-lg shadow-lg text-center">
             <h2 className="text-2xl font-medium mb-4">
                 Let’s create <br /> something together!
             </h2>
-            <button className="btn btn-sm">Contact</button>
+            <Link to="/contact">
+                <button className="btn btn-sm">Contact</button>
+            </Link>
         </section>
     );
 };
